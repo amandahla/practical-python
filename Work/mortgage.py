f502b9c9ef4b4b88fc7095270405540527e9c3d1
Amanda Hager Lopes de Andrade Katz <amanda.katz@serpro.gov.br>
2020-06-03 16:57:50 -0300

    Finished 1.3

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,28 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment_set = 2684.11
+total_paid = 0.0
+
+extra_payment_start_month = 60
+extra_payment_end_month = 108
+extra_payment = 1000
+
+months = 1
+while principal > 0:
+    payment = payment_set
+    if months >= extra_payment_start_month and months <= extra_payment_end_month:
+        payment = payment_set + extra_payment
+
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+
+    if principal > 0:
+        print(months,total_paid, principal)
+    
+    months += 1
+
+print('Total paid', round(total_paid,1))
+print('Months', months-1)
